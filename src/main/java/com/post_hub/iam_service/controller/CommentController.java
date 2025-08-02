4604a44473a97a895b06d8b7f25b3c6964dbd49a
Demid Greshnikov <demid.westhill@gmail.com>
2025-08-02 21:09:51 +0200

    create connection with postgresql

--- src/main/java/com/post_hub/iam_service/controller/CommentController.java
+++ src/main/java/com/post_hub/iam_service/controller/CommentController.java
@@ -1,6 +1,7 @@
 package com.post_hub.iam_service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,16 +16,28 @@ import com.post_hub.iam_service.service.CommentService;
 @RequestMapping("/comments")
 public class CommentController {
     private final CommentService commentService;
+    private final CommentService commentServiceLog;
 
     @Autowired
-    public CommentController(CommentService commentService) {
+    public CommentController(
+        CommentService commentService,
+      @Qualifier("CommentServiceLog") CommentService commentServiceLog
+    ) {
         this.commentService = commentService;
+        this.commentServiceLog = commentServiceLog;
     }
 
     @PostMapping("/create")
     public ResponseEntity<String> createComment(@RequestBody CreateCommentRequest requestBody) {
         String comment = requestBody.getContent();
         this.commentService.createComment(comment);
+        return new ResponseEntity<String>("Comment have been created", HttpStatus.OK);
+    }
+
+    @PostMapping("/create/log")
+    public ResponseEntity<String> createCommentLog(@RequestBody CreateCommentRequest requestBody) {
+        String comment = requestBody.getContent();
+        this.commentServiceLog.createComment(comment);
         return new ResponseEntity<String>("Comment have been created", HttpStatus.OK);
     }
 }
--- src/main/java/com/post_hub/iam_service/service/Impl/CommentServiceLogImp.java
+++ src/main/java/com/post_hub/iam_service/service/Impl/CommentServiceLogImp.java
@@ -1,22 +1,23 @@
 package com.post_hub.iam_service.service.Impl;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import com.post_hub.iam_service.service.CommentService;
 
-@Service
-@Primary
-public class CommentServiceImp implements CommentService {
+@Service("CommentServiceLog")
+public class CommentServiceLogImp implements CommentService {
 
     private final List<String> comments = new ArrayList<>();
 
     @Override
     public void createComment(String comment) {
+        String log = String.format("[%s] create comment %s", LocalDateTime.now(), comment);
         this.comments.add(comment);
+        System.out.println(log);
     }
 
 }
