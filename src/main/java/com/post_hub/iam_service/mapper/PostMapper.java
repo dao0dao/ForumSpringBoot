727c4558ab42234e86b51f27890fd7339d3b1f2e
Demid Greshnikov <demid.westhill@gmail.com>
2025-08-15 23:01:50 +0200

    create like, dislike endpoints, create mapper

---
+++ src/main/java/com/post_hub/iam_service/mapper/PostMapper.java
@@ -0,0 +1,31 @@
+package com.post_hub.iam_service.mapper;
+
+import com.post_hub.iam_service.model.dto.post.PostDTO;
+import com.post_hub.iam_service.model.enteties.Post;
+import com.post_hub.iam_service.model.request.PostRequest;
+
+public class PostMapper {
+    private PostMapper() {
+    }
+
+    public static Post toEntity(PostRequest postRequest) {
+        if (postRequest == null) {
+            return null;
+        }
+        return Post.builder().title(postRequest.getTitle()).content(postRequest.getContent()).build();
+
+    }
+
+    public static PostDTO toDTO(Post post) {
+        if (post == null) {
+            return null;
+        }
+        return PostDTO.builder()
+                .content(post.getContent())
+                .created(post.getCreated())
+                .id(post.getId())
+                .likes(post.getLikes())
+                .title(post.getTitle())
+                .build();
+    }
+}
