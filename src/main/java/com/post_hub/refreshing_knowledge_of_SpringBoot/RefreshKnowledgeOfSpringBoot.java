6a3410fc6592096ee0c4f5f11eed9d638a40bedb
Demid Greshnikov <demid.westhill@gmail.com>
2025-12-20 22:28:00 +0100

    fix test class

--- src/main/java/com/post_hub/refreshing_knowledge_of_SpringBoot/RefreshKnowledgeOfSpringBoot.java
+++ src/main/java/com/post_hub/refreshing_knowledge_of_SpringBoot/RefreshKnowledgeOfSpringBoot.java
@@ -3,11 +3,11 @@ package com.post_hub.refreshing_knowledge_of_SpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class RefreshKnowledgeOfSpringBoot {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RefreshKnowledgeOfSpringBoot.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RefreshKnowledgeOfSpringBoot.class, args);
+    }
 }
