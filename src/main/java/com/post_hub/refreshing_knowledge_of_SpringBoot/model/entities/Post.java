9249cb66c9acb910960f5842f76c357a1f636ddf
Demid Greshnikov <demid.westhill@gmail.com>
2026-01-07 18:22:11 +0100

    remove unused imports

--- src/main/java/com/post_hub/refreshing_knowledge_of_SpringBoot/model/entities/Post.java
+++ src/main/java/com/post_hub/refreshing_knowledge_of_SpringBoot/model/entities/Post.java
@@ -6,7 +6,6 @@ import java.util.Set;
 
 import org.hibernate.annotations.Formula;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -14,7 +13,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
