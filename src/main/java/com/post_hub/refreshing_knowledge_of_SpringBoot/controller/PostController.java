1e75421adc991451d928de0b600b37d00af697fa
Demid Greshnikov <demid.westhill@gmail.com>
2026-06-11 22:41:22 +0200

    add serializable

--- src/main/java/com/post_hub/refreshing_knowledge_of_SpringBoot/controller/PostController.java
+++ src/main/java/com/post_hub/refreshing_knowledge_of_SpringBoot/controller/PostController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.post_hub.refreshing_knowledge_of_SpringBoot.model.constans.ApiLogMessage;
+import com.post_hub.refreshing_knowledge_of_SpringBoot.model.dto.comment.CommentDTO;
 import com.post_hub.refreshing_knowledge_of_SpringBoot.model.dto.post.PostDTO;
 import com.post_hub.refreshing_knowledge_of_SpringBoot.model.dto.post.PostSearchDTO;
 import com.post_hub.refreshing_knowledge_of_SpringBoot.model.request.post.PostRequest;
@@ -99,6 +100,15 @@ public class PostController {
         ApiResponse<PostDTO> response = ApiResponse.createSuccessful(createdPost);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @PostMapping("${end.points.id}${end.points.comment}")
+    public ResponseEntity<ApiResponse<CommentDTO>> createComment(@RequestBody String entity) {
+        //TODO: process POST request
+        
+        
+        return null;
+    }
+    
 
     @GetMapping("${end.points.all}")
     public ResponseEntity<ApiResponse<PaginationPayload<PostSearchDTO>>> getAllPosts(
--- src/main/java/com/post_hub/refreshing_knowledge_of_SpringBoot/model/dto/comment/CommentDTO.java
+++ src/main/java/com/post_hub/refreshing_knowledge_of_SpringBoot/model/dto/comment/CommentDTO.java
@@ -1,8 +1,7 @@
 package com.post_hub.refreshing_knowledge_of_SpringBoot.model.dto.comment;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
-
-import com.post_hub.refreshing_knowledge_of_SpringBoot.model.dto.user.UserDTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class CommentDTO {
+public class CommentDTO implements Serializable {
     
     private LocalDateTime created;
     private String message;
