2fa2a0838f3230b2ae141c466078004fa2c35e55
Demid Greshnikov <demid.westhill@gmail.com>
2026-06-15 22:07:59 +0200

    add validation at auth

--- src/main/java/com/post_hub/refreshing_knowledge_of_SpringBoot/controller/AuthorizationController.java
+++ src/main/java/com/post_hub/refreshing_knowledge_of_SpringBoot/controller/AuthorizationController.java
@@ -12,16 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.post_hub.refreshing_knowledge_of_SpringBoot.model.constans.ApiLogMessage;
 import com.post_hub.refreshing_knowledge_of_SpringBoot.model.dto.user.UserProfileDTO;
-import com.post_hub.refreshing_knowledge_of_SpringBoot.model.request.authorization.AuthorizationRequest;
+import com.post_hub.refreshing_knowledge_of_SpringBoot.model.request.authorization.LoginRequest;
+import com.post_hub.refreshing_knowledge_of_SpringBoot.model.request.authorization.RegisterRequest;
 import com.post_hub.refreshing_knowledge_of_SpringBoot.model.response.ApiResponse;
 import com.post_hub.refreshing_knowledge_of_SpringBoot.security.model.constans.SecurityConstans;
 import com.post_hub.refreshing_knowledge_of_SpringBoot.service.AuthorizationsService;
 import com.post_hub.refreshing_knowledge_of_SpringBoot.service.models.AuthResult;
 import com.post_hub.refreshing_knowledge_of_SpringBoot.utils.ApiUtils;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 
 @Slf4j
 @RestController
@@ -29,44 +30,47 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping()
 public class AuthorizationController {
 
-    private final AuthorizationsService authorizationsService;
+        private final AuthorizationsService authorizationsService;
 
-    @PostMapping("${end.points.login}")
-    public ResponseEntity<ApiResponse<UserProfileDTO>> authorizeUser(@RequestBody AuthorizationRequest authRequest) {
-        log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
+        @PostMapping("${end.points.login}")
+        public ResponseEntity<ApiResponse<UserProfileDTO>> authorizeUser(@Valid @RequestBody LoginRequest authRequest) {
+                log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
 
-        AuthResult authResult = this.authorizationsService.loginUser(authRequest.getEmail(),
-                authRequest.getPassword());
+                AuthResult authResult = this.authorizationsService.loginUser(authRequest.getEmail(),
+                                authRequest.getPassword());
 
-        ApiResponse<UserProfileDTO> body = ApiResponse.tokenCreateUpdated(authResult.getUserProfile());
-        return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE,
-                        ApiUtils.createCookie(SecurityConstans.JWT_COOKIE_NAME, authResult.getToken()))
-                .body(body);
-    }
+                ApiResponse<UserProfileDTO> body = ApiResponse.tokenCreateUpdated(authResult.getUserProfile());
+                return ResponseEntity.ok()
+                                .header(HttpHeaders.SET_COOKIE,
+                                                ApiUtils.createCookie(SecurityConstans.JWT_COOKIE_NAME,
+                                                                authResult.getToken()))
+                                .body(body);
+        }
 
-    @PostMapping("${end.points.register}")
-    public ResponseEntity<Void> registerUser(@RequestBody AuthorizationRequest authRequest) {
-        log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
+        @PostMapping("${end.points.register}")
+        public ResponseEntity<Void> registerUser(@Valid @RequestBody RegisterRequest authRequest) {
+                log.trace(ApiLogMessage.NAME_OF_CURRENT_METHOD.getValue(), ApiUtils.getMethodName());
 
-        this.authorizationsService.registerUser(authRequest.getEmail(), authRequest.getPassword(),
-                authRequest.getConfirmPassword());
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
+                this.authorizationsService.registerUser(authRequest.getEmail(), authRequest.getPassword(),
+                                authRequest.getConfirmPassword());
+                return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
 
-    @GetMapping("${end.points.token}${end.points.refresh}")
-    public ResponseEntity<Void> getMethodName() {
-        String token = this.authorizationsService.refreshToken();
-        return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, ApiUtils.createCookie(SecurityConstans.JWT_COOKIE_NAME, token)).build();
-    }
+        @GetMapping("${end.points.token}${end.points.refresh}")
+        public ResponseEntity<Void> getMethodName() {
+                String token = this.authorizationsService.refreshToken();
+                return ResponseEntity.ok()
+                                .header(HttpHeaders.SET_COOKIE,
+                                                ApiUtils.createCookie(SecurityConstans.JWT_COOKIE_NAME, token))
+                                .build();
+        }
 
-    @PostMapping("${end.points.logout}")
-    public ResponseEntity<Void> logout() {
-        this.authorizationsService.logoutUser();
-        return ResponseEntity.status(HttpStatus.OK)
-                .header(HttpHeaders.SET_COOKIE, ApiUtils.deleteCookie(SecurityConstans.JWT_COOKIE_NAME))
-                .build();
-    }
+        @PostMapping("${end.points.logout}")
+        public ResponseEntity<Void> logout() {
+                this.authorizationsService.logoutUser();
+                return ResponseEntity.status(HttpStatus.OK)
+                                .header(HttpHeaders.SET_COOKIE, ApiUtils.deleteCookie(SecurityConstans.JWT_COOKIE_NAME))
+                                .build();
+        }
 
 }
